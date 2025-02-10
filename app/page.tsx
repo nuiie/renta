5f25ff9df8d38ca071ac84003fbbf0ccf45ee318
nui <ppnuie@gmail.com>
2025-02-11 03:11:54 +0700

    add badge

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import { getAllProperty, Property } from "../lib/airtable"
+import { getAllProperty, Property, getAllContract } from "../lib/airtable"
 import { Badge } from "@/components/ui/badge"
 
 export default async function Home() {
   const allProperty: Property[] = (await getAllProperty()) as Property[]
+  const allContract = await getAllContract()
+
+  const propertyWithContract = mapContractToProperty(allContract, allProperty)
 
   // todo
   // 1. red, green property text decided by ongoing contract
@@ -13,11 +16,47 @@ export default async function Home() {
       <h1>Dashboard</h1>
       <p>all property</p>
       <ul>
-        {allProperty?.map((p) => (
-          <li key={p.id}>{`${p.no} ${p.desc}`}</li>
-        ))}
-      </ul>{" "}
-      o
+        {/* {allProperty?.map((p) => (
+          <li key={p.id}>{`${p.no} - ${p.desc}`}</li>
+        ))} */}
+        {Object.keys(propertyWithContract).map((key) => {
+          return (
+            <li key={key}>
+              {`${propertyWithContract[key].no} - ${propertyWithContract[key].desc}`}{" "}
+              <BadgeGroup contract={propertyWithContract[key].contract} />
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
+}
+
+function BadgeGroup({ contract }) {
+  return Object.keys(contract).map((key) => (
+    <Badge
+      key={key}
+      variant={contract[key].status == "Ongoing" ? "default" : "secondary"}
+    >
+      {`${contract[key].Rent.toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+      })} [${contract[key]["start date"]}]`}
+    </Badge>
+  ))
+}
+
+function mapContractToProperty(contract, property) {
+  // put property to object, key is airtable id of property
+  // put contract to object by key
+  const newProperty = {}
+  property.map((p) => {
+    const { airtableId, ...rest } = p
+    newProperty[airtableId] = { ...rest, contract: {} }
+  })
+
+  contract.map((c) => {
+    const { property, airtableId, ...rest } = c
+    newProperty[property[0]]["contract"][airtableId] = { ...rest }
+  })
+  return newProperty
 }
--- lib/airtable.ts
+++ lib/airtable.ts
@@ -1,5 +1,4 @@
 import Airtable from "airtable"
-import { log } from "console"
 
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
@@ -8,6 +7,7 @@ Airtable.configure({
 const base = Airtable.base("appBNQZ6kc8ziiDRA")
 
 export interface Property {
+  airtableId: number
   id: number
   no?: string
   desc?: string
@@ -23,11 +23,27 @@ export const getAllProperty = () => {
     .firstPage()
     .then((records) => {
       const res: Property[] = records?.map((r) => ({
+        airtableId: r.getId(),
         id: r.fields.id,
         no: r.fields["House No."],
         desc: r.fields.Description,
       }))
       res.sort((a, b) => a.id - b.id)
+      return res
+    })
+}
+
+export const getAllContract = () => {
+  return base("contract")
+    .select({
+      fields: ["property", "Rent", "status", "start date"],
+    })
+    .firstPage()
+    .then((records) => {
+      const res = records?.map((r) => ({
+        airtableId: r.getId(),
+        ...r.fields,
+      }))
       return res
     })
 }
