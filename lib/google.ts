69c2e9fb9a5a0291629442a469e39e94e09bc937
nui <ppnuie@gmail.com>
2025-02-28 02:43:44 +0700

    mess with google

--- lib/google.ts
+++ lib/google.ts
@@ -1,17 +1,13 @@
-import { NextApiRequest, NextApiResponse } from "next"
 import { google } from "googleapis"
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function getTransaction() {
   try {
     const apiKey = process.env.GOOGLE_SHEETS_API_KEY // Store API key in .env.local
     const sheetId = "1mdj7QGLWoRbLCb6mKqlarUeKvhxo5IzryEMCmiLxHss" // Replace with your actual Google Sheet ID
     const range = "Sheet1!A2:H" // Change to match your range
 
     if (!apiKey) {
-      return res.status(500).json({ error: "Google Sheets API key is missing" })
+      return "Google Sheets API key is missing"
     }
 
     const sheets = google.sheets({ version: "v4", auth: apiKey })
@@ -21,9 +17,8 @@ export default async function handler(
       range,
     })
 
-    res.status(200).json({ data: response.data.values || [] })
+    return response
   } catch (error) {
     console.error("Error fetching Google Sheets data:", error)
-    res.status(500).json({ error: "Failed to fetch data from Google Sheets" })
   }
 }
