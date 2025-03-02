869661d3cb11e9f4dccd6bad1fd0cbcee2dac77a
nui <ppnuie@gmail.com>
2025-03-03 02:50:45 +0700

    error boundary fetch dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,36 @@
+import React, { Suspense } from "react"
 import {
   Property,
   Revenue,
   LatePayment,
   Transaction,
 } from "@/components/dashboard"
-import { Suspense } from "react"
+import ErrorBoundary from "@/components/ErrorBoundary"
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="mx-6 max-w-lg">
       <div className="grid grid-cols-1 gap-4">
-        <Suspense fallback={<p>loading properties...</p>}>
-          <Property />
-        </Suspense>
-        <Suspense fallback={<p>loading revenue...</p>}>
-          <Revenue />
-        </Suspense>
-        <Suspense fallback={<p>loading latepayment...</p>}>
-          <LatePayment />
-        </Suspense>
-        <Suspense fallback={<p>loading transaction...</p>}>
-          <Transaction />
-        </Suspense>
+        <ErrorBoundary fallback={<p>Error loading properties.</p>}>
+          <Suspense fallback={<p>Loading properties...</p>}>
+            <Property />
+          </Suspense>
+        </ErrorBoundary>
+        <ErrorBoundary fallback={<p>Error loading revenue.</p>}>
+          <Suspense fallback={<p>Loading revenue...</p>}>
+            <Revenue />
+          </Suspense>
+        </ErrorBoundary>
+        <ErrorBoundary fallback={<p>Error loading late payments.</p>}>
+          <Suspense fallback={<p>Loading late payments...</p>}>
+            <LatePayment />
+          </Suspense>
+        </ErrorBoundary>
+        <ErrorBoundary fallback={<p>Error loading transactions.</p>}>
+          <Suspense fallback={<p>Loading transactions...</p>}>
+            <Transaction />
+          </Suspense>
+        </ErrorBoundary>
       </div>
     </div>
   )
---
+++ app/property/page.tsx
@@ -0,0 +1,3 @@
+export default function Property() {
+  return <section className="px-6">property</section>
+}
