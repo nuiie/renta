b3b75281aabc6822ba14e5b5739319ff29221cc9
nui <ppnuie@gmail.com>
2025-03-06 00:00:58 +0700

    draft property detail page

--- app/property/[id]/page.tsx
+++ app/property/[id]/page.tsx
@@ -4,5 +4,13 @@ export default async function PropertyDetail({
   params: Promise<{ id: number }>
 }) {
   const { id } = await params
-  return <section className="px-6">property details {id}</section>
+  return (
+    <section className="px-6">
+      property details {id}
+      <p>header, nickname, button for edit, menu</p>
+      <p>Carousel</p>
+      <p>property table form</p>
+      <p>contract list</p>
+    </section>
+  )
 }
