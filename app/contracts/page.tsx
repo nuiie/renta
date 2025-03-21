ed36287aa86e46cfd2d69af5069cd4bc5ca625af
nui <ppnuie@gmail.com>
2025-03-21 15:45:33 +0700

    use modal

--- app/contracts/page.tsx
+++ app/contracts/page.tsx
@@ -4,15 +4,29 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { ChevronRight, Plus } from "lucide-react"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function Contracts() {
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold">Contracts</h1>
-        <Button size="sm" className="gap-1">
-          <Plus className="h-4 w-4" /> New
-        </Button>
+        <div className="space-x-2">
+          <Button size="sm" className="gap-1" variant="outline">
+            <Plus className="h-4 w-4" /> New Contract
+          </Button>
+          <DialogDemo />
+        </div>
       </div>
 
       <div className="flex items-center space-x-2">
@@ -85,5 +99,42 @@ export default function Contracts() {
         ))}
       </div>
     </div>
+  )
+}
+
+function DialogDemo() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size="sm" className="gap-1">
+          <Plus className="h-4 w-4" /> New Payment
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&#39;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
